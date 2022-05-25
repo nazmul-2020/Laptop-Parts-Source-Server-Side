58cb639d02f99d65339efcf972f4dc6266757561
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-05-25 10:43:10 +0600

    GET method products route

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mg2zq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nkbdl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -22,18 +18,18 @@ async function run() {
 
     try {
         await client.connect();
-        const serviceCollection = client.db("Laptop-Parts-source").collection("products");
+        const productsCollection = client.db("Laptop-Parts-source").collection("products");
+        const ReviewsCollection = client.db("Laptop-Parts-source").collection("reviews");
 
 
 
 
-        // // GET method route
-        // app.get('/service', async (req, res) => {
-        //     const query = {};
-        //     const cursor = serviceCollection.find(query).project({ name: 1 });
-        //     const services = await cursor.toArray();
-        //     res.send(services)
-        // })
+        // GET method products route
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const products =await productsCollection.find(query).toArray();
+            res.send(products)
+        })
 
 
 
