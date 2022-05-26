8db20a1c8e2d700b1a0355d01b1d773e74a0fdbd
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-05-26 12:50:58 +0600

    GET method admin/:email route

--- index.js
+++ index.js
@@ -58,6 +58,14 @@ async function run() {
             res.send(users)
         })
 
+        // GET method admin/:email route
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        })
+
         app.put('/user/admin/:email', async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
